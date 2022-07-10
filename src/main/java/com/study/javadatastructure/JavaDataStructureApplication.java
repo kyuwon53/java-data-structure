ff633fa6ff86960392d8afb262eb39e4b76b56f0
kyuwon <kyuwoon369@gmail.com>
2022-07-10 23:54:34 +0900

    Initial commit

---
+++ src/main/java/com/study/javadatastructure/JavaDataStructureApplication.java
@@ -0,0 +1,13 @@
+package com.study.javadatastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDataStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDataStructureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/javadatastructure/JavaDataStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.javadatastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDataStructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
